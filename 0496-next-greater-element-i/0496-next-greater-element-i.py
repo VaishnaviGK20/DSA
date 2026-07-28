a52f0822af04282f4d193f9cbeffee86c15ec956
VaishnaviGK20 <vaishnavivaishnavi500@gmail.com>
2026-07-28 21:43:17 +0530

    Time: 214 ms (5.09%), Space: 12.5 MB (59.73%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.py
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -9,20 +9,18 @@ class Solution(object):
         r = []
 
         for i in range(len(nums1)):
-            found = False
-
+            f=False
             for j in range(len(nums2)):
                 if nums1[i] == nums2[j]:
 
-                    for k in range(j + 1, len(nums2)):
+                    for k in range(j+1, len(nums2)):
                         if nums2[k] > nums2[j]:
                             r.append(nums2[k])
-                            found = True
+                            f=True
                             break
-
-                    if not found:
+                    if not f:
                         r.append(-1)
-
-                    break
-
         return r
+                        
+                    
+                    
