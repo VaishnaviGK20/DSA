7ed951de766bf800b7f466230370551432f100da
VaishnaviGK20 <vaishnavivaishnavi500@gmail.com>
2026-08-17 08:55:43 +0530

    Time: 11 ms (31.41%), Space: 12.5 MB (57.99%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -1,26 +1,25 @@
-class Solution(object):
-    def addTwoNumbers(self, l1, l2):
+class Solution(object): 
+    def addTwoNumbers(self, l1, l2): 
+        d=ListNode(0)
+        c=d
+        ca=0
 
-        dummy = ListNode(0)
-        curr = dummy
-        carry = 0
-
-        while l1 or l2 or carry:
-
+        while l1 or l2 or ca:
             a = l1.val if l1 else 0
-            b = l2.val if l2 else 0
+            b= l2.val if l2 else 0
 
-            s = a + b + carry
+            s=a+b+ca
+            c.next=ListNode(s%10)
+            ca=s//10
 
-            curr.next = ListNode(s % 10)
-            carry = s // 10
-
-            curr = curr.next
+            c=c.next
 
             if l1:
-                l1 = l1.next
-
+                l1=l1.next
             if l2:
-                l2 = l2.next
+                l2=l2.next
+        return d.next
 
-        return dummy.next
+
+        
+ 
